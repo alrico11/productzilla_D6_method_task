11e564934909597c9bf63ee23a0ddea79d819a37
Alrico Rizki <alricosaiyan123@gmail.com>
2023-03-02 21:27:13 +0700

    TASK METHOD & FUNCTIONS

--- tugas.js
+++ tugas.js
@@ -3,7 +3,7 @@ function datadiri(nama, asal, suku) {
     console.log(`Nama Saya ${nama}, saya berasal dari ${asal} dan saya bersuku ${suku}`)
 }
 function kampus(asalkampus, jurusan) {
-    console.log(`Saya sedang berkuliah di ${asalkampus} dan di Jurusan ${jurusan}`)
+    console.log(`Saya sedang berkuliah di ${asalkampus} dan di Jurusan ${jurusan}`+"\n\n")
 }
 let nama = "Alrico",
     asal = "Kota Semarang",
@@ -19,34 +19,39 @@ let x = 2,
 y =4
 
 //1
+console.log("Jika X = 2 & y = 4, Maka")
 function f(x){
+    console.log("Hasil f(x) =  x^2")
     return x ** 2
 }
-const hasil = f(x)
-console.log(hasil)
+var hasil = f(x)
+console.log(hasil+"\n")
 //2
-function f(x,y){
+function a(x,y){
+    console.log("Hasil f(x) =  (x*2)+(y*2)")
     return(x*2)+(y*2)
 }
-const hasil1 = f(x,y)
-console.log(hasil1)
+var hasil1 = a(x,y)
+console.log(hasil1+"\n")
 //3
-function f(x,y){
+function b(x,y){
+    console.log("Hasil f(x) =  (x*y)/2 ")
     return (x*y)/2
 }
-const hasil2 = f(x,y)
-console.log(hasil2)
+var hasil2 = b(x,y)
+console.log(hasil2+"\n")
 //4
-function f(x,y){
+function c(x,y){
+    console.log("Hasil f(x) = x*y")
     return x*y
 }
-const hasil3 = f(x,y)
-console.log(hasil3)
+var hasil3 = c(x,y)
+console.log(hasil3+"\n")
 
 //5
-function f(x){
+function d(x){
+    console.log("Hasil f(x) = 22/7 *2*x ")
     return 22/7*2*x
 }
-const hasil4 = f(x,y)
-console.log(hasil4)
-
+var hasil4 = d(x,y)
+console.log(hasil4+"\n")
