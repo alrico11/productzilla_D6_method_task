c0fadae419a8eae905654a7ad48d1fdddc931ab4
Alrico Rizki <alricosaiyan123@gmail.com>
2023-03-03 15:14:15 +0700

    TASK D6 METHOD AND FUNCTION

---
+++ taskmethod.js
@@ -0,0 +1,228 @@
+//Membuat 20 Method Procedure
+
+//Nama
+let nama;
+function setNama(argNama) {
+    nama = argNama
+}
+
+function getNama() {
+    setNama("Alrico")
+    console.log(nama)
+    return nama
+}
+getNama()
+
+//Nama nama hewan
+let namaHewan, namaHewan1, namaHewan2, namaHewan3,namaHewan4,namaHewan5,namaHewan6,namaHewan7,namaHewan8,namaHewan9,namaHewan10,
+namaHewan11, namaHewan12,namaHewan13,namaHewan14,namaHewan15,namaHewan16,namaHewan17,namaHewan18
+
+//namaHewan
+function setNamaHewan(value){
+    namaHewan = value
+}
+function getNamaHewan(){
+    setNamaHewan("Semut")
+    console.log(namaHewan)
+    return namaHewan
+}
+getNamaHewan()
+
+//namaHewan1
+function setNamaHewan1(value){
+    namaHewan1 = value
+}
+function getNamaHewan1(){
+    setNamaHewan1("Gajah")
+    console.log(namaHewan1)
+    return namaHewan1
+}
+getNamaHewan1()
+
+//namaHewan2
+function setNamaHewan2(value){
+    namaHewan2 = value
+}
+function getNamaHewan2(){
+    setNamaHewan2("Kerbau")
+    console.log(namaHewan2)
+    return namaHewan2
+}
+getNamaHewan2()
+
+//namaHewan3
+function setNamaHewan3(value){
+    namaHewan3 = value
+}
+function getNamaHewan3(){
+    setNamaHewan3("Sapi")
+    console.log(namaHewan3)
+    return namaHewan3
+}
+getNamaHewan3()
+
+
+//namaHewan4
+function setNamaHewan4(value){
+    namaHewan4 = value
+}
+function getNamaHewan4(){
+    setNamaHewan4("Kambing")
+    console.log(namaHewan4)
+    return namaHewan4
+}
+getNamaHewan4()
+
+//namaHewan
+function setNamaHewan5(value){
+    namaHewan5 = value
+}
+function getNamaHewan5(){
+    setNamaHewan5("Cicak")
+    console.log(namaHewan5)
+    return namaHewan5
+}
+getNamaHewan5()
+
+//namaHewan
+function setNamaHewan6(value){
+    namaHewan6 = value
+}
+function getNamaHewan6(){
+    setNamaHewan6("Singa")
+    console.log(namaHewan6)
+    return namaHewan6
+}
+getNamaHewan6()
+
+//namaHewan
+function setNamaHewan7(value){
+    namaHewan7 = value
+}
+function getNamaHewan7(){
+    setNamaHewan7("Serigala")
+    console.log(namaHewan7)
+    return namaHewan7
+}
+getNamaHewan7()
+
+//namaHewan
+function setNamaHewan8(value){
+    namaHewan8 = value
+}
+function getNamaHewan8(){
+    setNamaHewan8("Lumba-Lumba")
+    console.log(namaHewan8)
+    return namaHewan8
+}
+getNamaHewan8()
+
+// //namaHewan
+function setNamaHewan9(value){
+    namaHewan9 = value
+}
+function getNamaHewan9(){
+    setNamaHewan9("Paus")
+    console.log(namaHewan9)
+    return namaHewan9
+}
+getNamaHewan9()
+
+//namaHewan
+function setNamaHewan10(value){
+    namaHewan10 = value
+}
+function getNamaHewan10(){
+    setNamaHewan10("Udang")
+    console.log(namaHewan10)
+    return namaHewan10
+}
+getNamaHewan10()
+
+// //namaHewan
+function setNamaHewan11(value){
+    namaHewan11 = value
+}
+function getNamaHewan11(){
+    setNamaHewan11("Siput")
+    console.log(namaHewan11)
+    return namaHewan11
+}
+getNamaHewan11()
+
+//namaHewan
+function setNamaHewan12(value){
+    namaHewan12 = value
+}
+function getNamaHewan12(){
+    setNamaHewan12("Ular")
+    console.log(namaHewan12)
+    return namaHewan12
+}
+getNamaHewan12()
+
+//namaHewan
+function setNamaHewan13(value){
+    namaHewan13 = value
+}
+function getNamaHewan13(){
+    setNamaHewan13("Macan")
+    console.log(namaHewan13)
+    return namaHewan13
+}
+getNamaHewan13()
+
+//namaHewan
+function setNamaHewan14(value){
+    namaHewan14 = value
+}
+function getNamaHewan14(){
+    setNamaHewan14("Babi")
+    console.log(namaHewan14)
+    return namaHewan14
+}
+getNamaHewan14()
+
+//namaHewan
+function setNamaHewan15(value){
+    namaHewan15 = value
+}
+function getNamaHewan15(){
+    setNamaHewan15("Anjing")
+    console.log(namaHewan15)
+    return namaHewan15
+}
+getNamaHewan15()
+
+//namaHewan
+function setNamaHewan16(value){
+    namaHewan16 = value
+}
+function getNamaHewan16(){
+    setNamaHewan16("Kucing")
+    console.log(namaHewan16)
+    return namaHewan16
+}
+getNamaHewan16()
+
+//namaHewan
+function setNamaHewan17(value){
+    namaHewan17 = value
+}
+function getNamaHewan17(){
+    setNamaHewan17("Kecoa")
+    console.log(namaHewan17)
+    return namaHewan17
+}
+getNamaHewan17()
+
+//namaHewan
+function setNamaHewan18(value){
+    namaHewan18 = value
+}
+function getNamaHewan18(){
+    setNamaHewan18("Hiu")
+    console.log(namaHewan18)
+    return namaHewan18
+}
+getNamaHewan18()
